e9c357dc354e703ec9043c6632824cbd00e9bd64
Duncan Steenburgh <duncan@sproutsocial.com>
2016-06-19 16:44:47 -0500

    Added restaurant object, added hack to fix restaurant abbreviations

---
+++ Restaurant.py
@@ -0,0 +1,55 @@
+import datetime
+
+
+class Restaurant:
+    def __init__(self, name):
+        self.name = name
+        self._times = []
+        self._avg_seconds = None
+
+    def get_average_time(self):
+        # Set to none by add_time()
+        # allows caching of average time
+        if self._avg_seconds is None:
+            avg_seconds = sum(self._times) / len(self._times)
+        return avg_seconds
+
+    def add_time(self, timestamp_str):
+        self._times.append(convert_from_timestamp(timestamp_str))
+        self._avg_seconds = None
+
+    def __hash__(self):
+        return self.name.__hash__()
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __str__(self):
+        return str(len(self._times)) + " | " + self.name + " | " + format_seconds(self.get_average_time())
+
+
+def convert_from_timestamp(timestamp_str):
+    dt_obj = datetime.datetime.fromtimestamp(int(float(timestamp_str)))
+    hour = dt_obj.hour
+    minutes = dt_obj.minute
+    seconds = dt_obj.second
+    return hour*3600 + minutes*60 + seconds
+
+
+# format seconds to 12 hours format
+# Ex. 49204 => '1:40:04 P.M.'
+def format_seconds(sec):
+    # Ex. td = "11:52:12"
+    td = str(datetime.timedelta(seconds=sec))
+    td_split = td.split(":")
+    (hours, minutes, seconds) = (int(td_split[0]), int(td_split[1]), int(td_split[2]))
+    if hours >= 12:
+        period = " P.M."
+    else:
+        period = " A.M."
+    if hours > 12:
+        hours -= 12
+        td = str(hours) + td[2:]
+    if hours == 0:
+        hours = 12
+    return td + period
--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import re
 import datetime
 
 import API
+from Restaurant import Restaurant
 
 LUNCH_DELIVERY = "C04FQP1G4"
 
 
 def main():
     restaurants = {}
-    # resp_json = API.post_message('test_dininginbot', 'test')
     resp_json = API.get_channel_history(channelID=LUNCH_DELIVERY, count=1000)
 
     # Using two separate regexes will help us identify restaurants with 'and' in their name more reliably
@@ -27,80 +27,65 @@ def main():
 
     for msg in single_rest_msgs:
         rest_name = single_rest_regex.match(msg['text']).group(1).strip()
+        rest_name = clean_restaurant_name(rest_name)
         add_restaurant(restaurants, rest_name, msg)
 
     for msg in multiple_rest_msgs:
         rest_name_str = multiple_rest_regex.match(msg['text']).group(1)
         # Regex note: & signs come in through the API as &amp;
         rest_name_list = re.split("(?i),\s?(?:and|&amp;)?\s|\s(?:and|&amp;)\s", rest_name_str)
+        rest_name_list = clean_rest_list(rest_name_list)
         for rest_name in rest_name_list:
             add_restaurant(restaurants, rest_name, msg)
 
-    map_to_averages(restaurants)
-    map_dict(format_seconds, restaurants)
-
-    # map_dict(lambda L: len(L), restaurants)
-
-    pretty_print_dictionary(restaurants)
+    rests_list = restaurants.values()
+    rests_list.sort(key=lambda rest: rest.name, reverse=False)
+    for rest in rests_list:
+        print rest
 
 
-# format seconds to 12 hours format
-# Ex. 49204 => '1:40:04 P.M.'
-def format_seconds(sec):
-    # Ex. td = "11:52:12"
-    td = str(datetime.timedelta(seconds=sec))
-    td_split = td.split(":")
-    (hours, minutes, seconds) = (int(td_split[0]), int(td_split[1]), int(td_split[2]))
-    if hours >= 12:
-        period = " P.M."
-    else:
-        period = " A.M."
-    if hours > 12:
-        hours -= 12
-        td = str(hours) + td[2:]
-    if hours == 0:
-        hours = 12
-    return td + period
+def clean_rest_list(rest_name_list):
+    # Special case
+    if rest_name_list.__contains__("the burger"):
+        rest_name_list.remove("the burger")
+
+    rest_name_list = map(clean_restaurant_name, rest_name_list)
+    return rest_name_list
 
 
-# applies map_fn to each value stored in dictionary
-def map_dict(map_fn, dict):
-    for (key, val) in dict.iteritems():
-        dict[key] = map_fn(val)
-
-
-# converts dict of unix timestamp list to average seconds
-def map_to_averages(dict):
-    for (key, val) in dict.iteritems():
-        dict[key] = reduce(lambda x, y: x+y, map(convert_from_timestamp, val)) / len(val)
-
-
-# converts unix timestamp to seconds (in 24 hour period)
-# Ex. "1456768776.000113" => 43176
-def convert_from_timestamp(timestamp_str):
-    dt_obj = datetime.datetime.fromtimestamp(int(float(timestamp_str)))
-    hour = dt_obj.hour
-    minutes = dt_obj.minute
-    seconds = dt_obj.second
-    return hour*3600 + minutes*60 + seconds
-
-
-def clean_restaurant_name(restaurant):
+def clean_restaurant_name(rest_name):
     # convert letters to lower case
-    restaurant = restaurant.lower()
+    rest_name = rest_name.lower()
     # replace '&'' with 'and'
-    restaurant = restaurant.replace("amp", "and")
+    rest_name = rest_name.replace("amp", "and")
     # remove special chars (only keep numbers, letters, and spaces)
-    restaurant = re.sub('[^a-zA-Z0-9 \n\.]', '', restaurant)
-    return restaurant
+    rest_name = re.sub('[^a-zA-Z0-9 \n\.]', '', rest_name)
+
+    replacements = {
+        "idof": "i dream of falafel",
+        "mannys": "mannys deli",
+        "sro": "standing room only",
+        "tgab": "the great american bagel",
+        "gab": "the great american bagel",
+        "stax": "stax cafe",
+        "uno": "unos",
+        "ginos": "ginos east",
+    }
+    if rest_name in replacements.keys():
+        rest_name = replacements[rest_name]
+
+    return rest_name
 
 
-def add_restaurant(rest_list, restaurant, msg):
-    restaurant = clean_restaurant_name(restaurant)
-    # print rest_list.get(restaurant, [])
-    # print restaurant
-    timestamp = msg['ts']
-    rest_list[restaurant] = rest_list.get(restaurant, []) + [timestamp]
+def add_restaurant(rest_list, rest_name, msg):
+    restaurant = Restaurant(name=rest_name)
+    timestamp_str = msg['ts']
+
+    if rest_list.__contains__(restaurant):
+        rest_list[restaurant].add_time(timestamp_str)
+    else:
+        restaurant.add_time(timestamp_str)
+        rest_list[restaurant] = restaurant
 
 
 def pretty_print_dictionary(dict):
@@ -108,3 +93,4 @@ def pretty_print_dictionary(dict):
 
 if __name__ == "__main__":
     main()
+
