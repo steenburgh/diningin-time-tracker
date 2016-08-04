99ae5ea19067cce1e8ff41d84851c1e37312822c
Duncan Steenburgh <duncan@sproutsocial.com>
2016-08-04 18:09:33 -0500

    Update Sample_Credentials.py
    Oops...

--- Sample_Credentials.py
+++ Sample_Credentials.py
@@ -1,3 +1,3 @@
-# This is a sample config file. Make a copy of this and rename it to Credentialsls.py
+# This is a sample config file. Make a copy of this and rename it to Credentials.py
 # You can generate a slack AUTH_TOKEN here: https://api.slack.com/docs/oauth-test-tokens
 AUTH_TOKEN = 'xoxb-0000000000000000-somelongstringofotherstuff'
