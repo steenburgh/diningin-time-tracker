0d3c0abffce1721df36739faa9a3864011ceca35
Duncan Steenburgh <duncan@sproutsocial.com>
2016-08-04 18:06:09 -0500

    Add api key link to Sample_Credentials.py

--- Sample_Credentials.py
+++ Sample_Credentials.py
@@ -1,2 +1,3 @@
 # This is a sample config file. Make a copy of this and rename it to Credentialsls.py
+# You can generate a slack AUTH_TOKEN here: https://api.slack.com/docs/oauth-test-tokens
 AUTH_TOKEN = 'xoxb-0000000000000000-somelongstringofotherstuff'
